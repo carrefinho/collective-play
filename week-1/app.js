3f585f8c5134ed8da65d8c0464ea11ef50548b75
carrefinho <winlxs@gmail.com>
2019-09-05 13:07:51 -0400

    bamba

--- week-1/app.js
+++ week-1/app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+    // res.writeHead(200);
     res.send('hello world');
-})
+});
 
-app.listen(4000);
+app.listen(4000, () => {
+    console.log('listening on port 4000');
+});
