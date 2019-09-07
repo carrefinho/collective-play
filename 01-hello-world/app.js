3a8201dad445f25ab283107daa98a5895c8dfc2e
carrefinho <winlxs@gmail.com>
2019-09-06 22:36:46 -0400

    shooter

--- 01-hello-world/app.js
+++ 01-hello-world/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.sendFile(__dirname + '/pages/shooter.html');
 });
 
 app.listen(4000, () => {
