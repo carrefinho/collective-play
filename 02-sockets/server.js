aa9b5d385b0ad4e58b517e93cabfba26a5b04d1c
carrefinho <winlxs@gmail.com>
2019-09-12 12:41:46 -0400

    sockets hw

--- 02-sockets/server.js
+++ 02-sockets/server.js
@@ -1,4 +1,4 @@
-const port = process.env.PORT || 8000;
+const port = 8000;
 const express = require('express');
 let app = express();
 
@@ -7,4 +7,20 @@ let server = require('http').createServer(app).listen(port, () => {
 })
 
 app.use(express.static('public'));
+
+const io = require('socket.io').listen(server);
+
+io.sockets.on('connection', (socket) => {
+
+    console.log('new client: ' + socket.id);
+
+    socket.on('data', (data) => {
+        console.log('date received');
+        io.sockets.emit('data', data);
+    })
+
+    socket.on('disconnect', () => {
+        console.log('disconnected ' + socket.id);
+    })
+})
 
